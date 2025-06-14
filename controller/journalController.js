2b4438d3617adbd1e4993f8231ff2cabc76e5e9c
Henry <Henries90@gmail.com>
2025-06-14 19:04:59 +0100

    Fix import

--- controller/journalController.js
+++ controller/journalController.js
@@ -2,6 +2,7 @@ const { Journal } = require("../model/Journal");
 
 const getCurrentIssue = async (req, res) => {
   const { name } = req.params;
+  if (!name) return res.status(500).json({ error: "Invalid journal name" });
   const journal = await Journal.find({ name });
   if (!journal) return res.status(404).json({ error: "Invalid journal name" });
   const issue = journal.issue;
@@ -14,7 +15,7 @@ const changeCurrentIssue = async (req, res) => {
   if (!journal) return res.status(404).json({ error: "Invalid journal name" });
   journal.issue = Number(issue);
   const result = journal.save();
-  res.json({ issue });
+  res.json({ result });
 };
 
 module.exports = { getCurrentIssue, changeCurrentIssue };
--- routes/acceptedManuscripts.js
+++ routes/acceptedManuscripts.js
@@ -3,3 +3,5 @@ const router = express.Router();
 const acceptedManuscriptController = require("../controller/acceptedManuscriptsController");
 
 router.route("/:name/:issue").get(acceptedManuscriptController.getByIssue);
+
+module.exports = router;
