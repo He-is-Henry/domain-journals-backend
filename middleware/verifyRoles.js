c703d20a3411ae7245641fd86866a41f8bfe5cdf
Henry <Henries90@gmail.com>
2025-06-16 08:48:01 +0100

    Fix typo

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -1,13 +1,13 @@
 const verifyRoles = (...allowedRoles) => {
   return (req, res, next) => {
-    if (!req?.roles)
+    if (!req?.role)
       return res
         .status(403)
         .json({ error: "User not authorized to perform this action" });
 
     const rolesArray = [...allowedRoles];
     console.log(rolesArray);
-    const userRoles = req.user.roles;
+    const userRoles = req.role;
 
     const hasRole = userRoles.some((role) => rolesArray.includes(role));
 
