f754840cafe65efe37f702e02edab3f69575aecb
Henry <Henries90@gmail.com>
2025-06-17 07:20:57 +0100

    Edited URL

--- routes/payment.js
+++ routes/payment.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const paymentController = require("../controller/paymentController");
-
-router.post("/:id", paymentController.payForManuscript);
-
 router.post(
   "/webhook",
   express.json({ type: "*/*" }),
   paymentController.confirmPayment
 );
+router.post("/manuscript/:id", paymentController.payForManuscript);
 
 module.exports = router;
