fb26f738b89d67b395433230f2749d6655ea7777
He-is-Henry <henries90@gmail.com>
2025-07-07 23:27:02 +0100

    Update allowList.js

--- config/allowList.js
+++ config/allowList.js
@@ -1,3 +1,3 @@
-const allowList = ["http://localhost:5173", "https://domain-journals.onrender.com", "https://domainjournals.com", "https://www.domainjournals.com/"];
+const allowList = ["http://localhost:5173", "https://domain-journals.onrender.com", "https://domainjournals.com", "https://www.domainjournals.com"];
 
 module.exports = allowList;
