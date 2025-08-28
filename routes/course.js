066585550d8942a80f065a5bdebf6de00d68162b
Henry <Henries90@gmail.com>
2025-08-28 21:16:00 +0100

    Added payment verification middleware to getCourse

--- routes/course.js
+++ routes/course.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const courseController = require("../controller/courseController");
 const verifyAdminJWT = require("../middleware/verifyAdminJWT");
 const verifyJWT = require("../middleware/verifyJWT");
+const verifyCoursePayment = require("../middleware/verifyCoursePayment");
 
 router.get("/", verifyJWT, courseController.getAllCourses);
 router.post("/", verifyAdminJWT, courseController.addCourse);
@@ -11,7 +12,7 @@ router.delete("/:paymentId", verifyAdminJWT, courseController.deletePayment);
 router.post("/pay/:course", verifyJWT, courseController.handleCoursePayment);
 router.patch("/:paymentId", verifyAdminJWT, courseController.confirmPayment);
 router.put("/:courseId", verifyAdminJWT, courseController.editCourse);
-router.get("/:courseId", courseController.getCourse);
+router.get("/:courseId", verifyCoursePayment, courseController.getCourse);
 router.get(
   "/admin/:courseId",
   verifyAdminJWT,
