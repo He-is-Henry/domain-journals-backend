0b20b618937b9524b2a604f3fa604aea313317bd
Henry <Henries90@gmail.com>
2025-10-09 09:46:28 +0100

    added exams

--- routes/draft.js
+++ routes/draft.js
@@ -6,3 +6,5 @@ const draftController = require("../controller/draftController");
 router.post("/:examId", verifyAuthorJWT, draftController.saveDraft);
 router.get("/:examId", verifyAuthorJWT, draftController.getDraft);
 router.patch("/:examId", verifyAuthorJWT, draftController.markCompleted);
+
+module.exports = router;
