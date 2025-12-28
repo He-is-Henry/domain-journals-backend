5c9b9b6338c46c3ea49afc5514419f4a66b71926
Henry <Henries90@gmail.com>
2025-12-29 00:35:20 +0100

    adjusted email settings... feels like this is going to be a long while 2

--- uttils/sendMail.js
+++ uttils/sendMail.js
@@ -1,10 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.zoho.com",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
