61dfa9ff02206505df37ae406577d1c2178c31d9
Henry <Henries90@gmail.com>
2026-04-09 23:11:02 +0100

    Added supabase get url

--- routes/supabase.js
+++ routes/supabase.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { uploadToMemory } = require("../uploads");
-const { uploadPdf, deletePdf } = require("../controller/supabaseController");
+const {
+  uploadPdf,
+  deletePdf,
+  getPdfUrl,
+} = require("../controller/supabaseController");
 
 router.post("/upload", uploadToMemory.single("file"), uploadPdf);
 router.delete("/delete", deletePdf);
+router.get("/url", getPdfUrl);
 
 module.exports = router;
